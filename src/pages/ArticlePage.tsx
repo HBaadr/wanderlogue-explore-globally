58375a6e24c1c2ab7fb43676927eede3cb70c460
Hourimeche Badr <badrhourimeche@gmail.com>
2025-09-08 13:26:46 +0100

    Update ArticlePage.tsx

--- src/pages/ArticlePage.tsx
+++ src/pages/ArticlePage.tsx
@@ -135,7 +135,7 @@ const ArticlePage = () => {
               </Button>
               <Button variant="outline" size="sm">
                 <Bookmark className="h-4 w-4 mr-2" />
-                Bookmark
+                {t('bookmark')}
               </Button>
             </div>
           </div>
